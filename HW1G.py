bc7b1b1033f0b1730e541267b79e39ad8141687f
zxpek <zx_pek@hotmail.com>
2016-11-18 17:35:47 +0000

    Add files via upload

--- HW1G.py
+++ HW1G.py
@@ -6,6 +6,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
+from networkx.drawing.nx_agraph import write_dot
+import pydotplus
+import pygraphviz
 
 G = nx.read_weighted_edgelist("HW1_problem1.txt", nodetype = str, create_using=nx.DiGraph())
 
@@ -15,6 +18,7 @@ print(adj.todense())
 inc = nx.incidence_matrix(G,nodelist=['a','b','c','d','e'],oriented=True)
 print(inc.todense())
 
+
 #spLength = pd.DataFrame.from_dict(nx.shortest_path_length(G))
 #print(spLength)
 
@@ -23,15 +27,22 @@ print(inc.todense())
 
 spLength = pd.DataFrame.from_dict(nx.floyd_warshall(G))
 print(spLength)
-
+write_dot(G,'hello.dot')
 #Finding diameter
-diam = 0
-for col in spLength:
-    for i in spLength[col]:
-        if i != np.inf and i >= diam:
-            diam = i
+def findDiam(G):
+    diam = 0
+    spLength = pd.DataFrame.from_dict(nx.floyd_warshall(G))
+    for col in spLength:
+        for i in spLength[col]:
+            if i != np.inf and i >= diam:
+                diam = i
+    
+    return diam 
 
-
+d = findDiam(G)    
+print('Diameter of Graph is ' + str(d))
+    
+#write_dot(G,'hello.dot')
 #Generate values for histogram
 deg=G.degree()
 values = [] #in same order as traversing keys
@@ -46,7 +57,7 @@ plt.title("Degree Distribution plot")
 plt.ylabel("Degree")
 plt.xlabel("Frequency")
 plt.savefig("degree_histogram.png")
-
+plt.close()
 #Check weak and strong connectivity
 print(nx.is_strongly_connected(G))
 print(nx.is_weakly_connected(G))
@@ -58,6 +69,8 @@ G2 = nx.from_numpy_matrix(G2)
 
 
 pos = nx.shell_layout(G2)
+plt.figure(figsize=(15,10))
+
 nx.draw(G2,pos)
 edge_weight=dict([((u,v,),int(d['weight'])) for u,v,d in G2.edges(data=True)])
 
@@ -67,3 +80,4 @@ nx.draw_networkx_edges(G2,pos)
 nx.draw_networkx_labels(G2,pos)
     
 plt.savefig("graph_plot.png")    
+plt.close()
