2b22b905c9456453351aabdd40793c2727d63105
zxpek <zx_pek@hotmail.com>
2016-11-21 16:09:55 +0000

    Add files via upload

--- HW1G.py
+++ HW1G.py
@@ -1,10 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Nov 17 15:03:17 2016
-
-Group 4
-
-Network Analytics HW1
 """
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -12,21 +8,28 @@ import networkx as nx
 import numpy as np
 
 
-G = nx.read_weighted_edgelist('HW1_problem1.txt', nodetype = str, create_using=nx.DiGraph())
+G = nx.read_weighted_edgelist("HW1_problem1.txt", nodetype = str, create_using=nx.DiGraph())
 
 adj = nx.adjacency_matrix(G, nodelist=['a','b','c','d','e'])
 print(adj.todense())
 
-inc = nx.incidence_matrix(G, nodelist=['a','b','c','d','e'], oriented=True)
+inc = nx.incidence_matrix(G,nodelist=['a','b','c','d','e'],oriented=True)
 print(inc.todense())
 
-#Print the shortest-path matrix between all pairs
+
+#spLength = pd.DataFrame.from_dict(nx.shortest_path_length(G))
+#print(spLength)
+
+#G.edges(data='weight')
+#edgeWeight=dict([((u,v,),int(d['weight'])) for u,v,d in G.edges(data=True)])
+
 distance_matrix = nx.floyd_warshall_numpy(G, nodelist=['a','b','c','d','e'], weight='weight')
 print(distance_matrix)
 
-#Find the longest shortest path in the graph
-def findDiam(dm):
+#Finding diameter
+def findDiam(G):
     diam = 0
+    dm = nx.floyd_warshall_numpy(G, nodelist=['a','b','c','d','e'], weight='weight')
     for i in range(dm.shape[0]):
         for j in range(dm.shape[1]):
             if dm[i,j] != np.inf and dm[i,j] >= diam:
@@ -34,35 +37,40 @@ def findDiam(dm):
     
     return diam 
 
-d = findDiam(distance_matrix)    
-print('The diameter of the graph is ' + str(d))
+d = findDiam(G)    
+print('Diameter of Graph is ' + str(d))
+    
+#write_dot(G,'hello.dot')
+#Generate values for histogram
+deg=G.degree()
+values = [] #in same order as traversing keys
+keys = [] #also needed to preserve order
+for key in deg.keys():
+  keys.append(key)
+  values.append(deg[key])
   
-#Plot the degree distribution of the graph
-degree_values = sorted(nx.degree(G).values())
-plt.hist(degree_values)
+#Plot histogram
+plt.hist(values)
 plt.title("Degree Distribution plot")
-plt.ylabel("Frequency")
-plt.xlabel("Degree")
+plt.ylabel("Degree")
+plt.xlabel("Frequency")
 plt.savefig("degree_histogram.png")
 plt.close()
-
 #Check weak and strong connectivity
 print(nx.is_strongly_connected(G))
 print(nx.is_weakly_connected(G))
 
-
-#Draw the graph
-#We removed the top half of the matrix in the txt file to keep 34 rows
+#Q2
 G2 = np.loadtxt('HW1_problem2.txt')
-G2 = np.matrix(G2)
+G2 = np.asmatrix(G2)
 G2 = nx.from_numpy_matrix(G2)
 
 
-pos = nx.spring_layout(G2)
+pos = nx.shell_layout(G2)
 plt.figure(figsize=(15,10))
 
 nx.draw(G2,pos)
-edge_weight=dict([((u,v),int(d['weight'])) for u,v,d in G2.edges(data=True)])
+edge_weight=dict([((u,v,),int(d['weight'])) for u,v,d in G2.edges(data=True)])
 
 nx.draw_networkx_edge_labels(G2,pos,edge_labels=edge_weight)
 nx.draw_networkx_nodes(G2,pos)
