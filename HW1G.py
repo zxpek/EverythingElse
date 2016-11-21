f9d9d37f9aef867daaebda14df3848eefe9a1e8f
msa416 <miram.atie16@imperial.ac.uk>
2016-11-21 15:52:28 +0000

    Update HW1G.py

--- HW1G.py
+++ HW1G.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Nov 17 15:03:17 2016
+
+Group 4
+
+Network Analytics HW1
 """
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -8,7 +12,7 @@ import networkx as nx
 import numpy as np
 
 
-G = nx.read_weighted_edgelist("HW1_problem1.txt", nodetype = str, create_using=nx.DiGraph())
+G = nx.read_weighted_edgelist('HW1_problem1.txt', nodetype = str, create_using=nx.DiGraph())
 
 adj = nx.adjacency_matrix(G, nodelist=['a','b','c','d','e'])
 print(adj.todense())
@@ -16,10 +20,11 @@ print(adj.todense())
 inc = nx.incidence_matrix(G, nodelist=['a','b','c','d','e'], oriented=True)
 print(inc.todense())
 
+#Print the shortest-path matrix between all pairs
 distance_matrix = nx.floyd_warshall_numpy(G, nodelist=['a','b','c','d','e'], weight='weight')
 print(distance_matrix)
 
-#Finding diameter
+#Find the longest shortest path in the graph
 def findDiam(dm):
     diam = 0
     for i in range(dm.shape[0]):
@@ -30,18 +35,9 @@ def findDiam(dm):
     return diam 
 
 d = findDiam(distance_matrix)    
-print('Diameter of Graph is ' + str(d))
-    
-#write_dot(G,'hello.dot')
-#Generate values for histogram
-deg=G.degree()
-values = [] #in same order as traversing keys
-keys = [] #also needed to preserve order
-for key in deg.keys():
-  keys.append(key)
-  values.append(deg[key])
+print('The diameter of the graph is ' + str(d))
   
-#Plot histogram
+#Plot the degree distribution of the graph
 degree_values = sorted(nx.degree(G).values())
 plt.hist(degree_values)
 plt.title("Degree Distribution plot")
@@ -49,13 +45,16 @@ plt.ylabel("Frequency")
 plt.xlabel("Degree")
 plt.savefig("degree_histogram.png")
 plt.close()
+
 #Check weak and strong connectivity
 print(nx.is_strongly_connected(G))
 print(nx.is_weakly_connected(G))
 
-#Q2
+
+#Draw the graph
+#We removed the top half of the matrix in the txt file to keep 34 rows
 G2 = np.loadtxt('HW1_problem2.txt')
-G2 = np.asmatrix(G2)
+G2 = np.matrix(G2)
 G2 = nx.from_numpy_matrix(G2)
 
 
@@ -63,7 +62,7 @@ pos = nx.shell_layout(G2)
 plt.figure(figsize=(15,10))
 
 nx.draw(G2,pos)
-edge_weight=dict([((u,v,),int(d['weight'])) for u,v,d in G2.edges(data=True)])
+edge_weight=dict([((u,v),int(d['weight'])) for u,v,d in G2.edges(data=True)])
 
 nx.draw_networkx_edge_labels(G2,pos,edge_labels=edge_weight)
 nx.draw_networkx_nodes(G2,pos)
